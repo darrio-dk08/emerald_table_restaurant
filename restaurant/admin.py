cc2a836fa085fd6b263442c65dc75318a671d79f
darrio-dk <darrio.dk@gmail.com>
2026-02-07 17:22:37 +0000

    Initial commit plus django library package installed

---
+++ restaurant/admin.py
@@ -0,0 +1,9 @@
+
+
+# Register your models here.
+
+from django.contrib import admin
+from .models import MenuItem, Booking
+
+admin.site.register(MenuItem)
+admin.site.register(Booking)
---
+++ restaurant/models.py
@@ -0,0 +1,24 @@
+
+
+# Create your models here.
+from django.db import models
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)  # Euros
+
+    def __str__(self):
+        return f"{self.name} - €{self.price}"
+
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    guests = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking for {self.name} on {self.date} at {self.time}"
