2c6b04d1b62e18ef9be321cabb5c0f1da4360936
darrio-dk <darrio.dk@gmail.com>
2026-02-07 17:24:06 +0000

    Add home, menu, booking, to views.py

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import MenuItem
+from .forms import BookingForm
+
+def home(request):
+    return render(request, 'restaurant/index.html')
+
+def menu(request):
+    items = MenuItem.objects.all()
+    return render(request, 'restaurant/menu.html', {'menu_items': items})
+
+def booking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = BookingForm()
+    return render(request, 'restaurant/booking.html', {'form': form})
