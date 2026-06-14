88cb7daa8e352d1e4cd1105f304470e7a390015e
darrio-dk <darrio.dk@outlook.ie>
2026-06-14 21:18:40 +0100

    Move secret key to environment variables

--- emerald_table_restaurant/settings.py
+++ emerald_table_restaurant/settings.py
@@ -9,24 +9,24 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = (
-    "django-insecure-zh0ouu!x2)_d2q_d#dx^buw+2+pwhy8%#batp03*5tlo-+u254"
-)
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
